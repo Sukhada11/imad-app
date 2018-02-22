70278a8fe107d94989183b4653d9420934be71d6
Sukhada Kulkarni <sukhadacoolkarni@gmail.com>
2018-02-22 13:01:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,60 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne={
+	title:'Article One | Sukhada Kulkarni',
+	heading:'Article One',
+	date:'February 19 2018',
+	content:
+ ` <p>
+               Content of first article.Content of first article.Content of first article.
+               Content of first article.Content of first article.Content of first article.
+				Content of first article.Content of first article.Content of first article.
+               Content of first article.Content of first article.Content of first article.
+   </p>
+   <p>
+               Content of first article.Content of first article.Content of first article.
+               Content of first article.Content of first article.Content of first article.
+				Content of first article.Content of first article.Content of first article.
+               Content of first article.Content of first article.Content of first article.
+  </p>
+    <p>
+               Content of first article.Content of first article.Content of first article.
+               Content of first article.Content of first article.Content of first article.
+				Content of first article.Content of first article.Content of first article.
+               Content of first article.Content of first article.Content of first article.
+  </p>`
+
+
+};
+var HTMLtemp=
+
+   `<html>
+    <head>
+        
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+           <div class="container">
+       <div>
+           <a href="/">Home</a>
+           
+       </div>
+       <hr/>
+       <h3>${heading}</h3>
+       <div>${date}</div>
+       <div>
+          ${content}
+       </div>
+       </div>
+    </body>
+</html>`
+
+   
+;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
