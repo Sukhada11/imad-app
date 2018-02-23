d81ae3c43fc5b57e43f6fea6ef2086a573d20d2f
Sukhada Kulkarni <sukhadacoolkarni@gmail.com>
2018-02-23 22:00:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var x=document.getElementById("text");
 x.innerHTML="New value"
 var img=document.getElementById("mandi");
 img.onclick=function(){
-  img.style.marginLeft='100px'  ;
+  var intrval=setInterval(moveRight,100);
 };
